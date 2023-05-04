bb4ce13321982e102823b72b544498ee03f3d64f
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-05-04 17:28:10 +0800

    chore: update 649

---
+++ problems/649. Dota2 Senate/index.spec.ts
@@ -0,0 +1,21 @@
+import { describe, expect, it } from 'vitest';
+import { predictPartyVictory } from '.';
+
+describe('predictPartyVictory', () => {
+  const testCases = [
+    { input: 'RD', expected: 'Radiant' },
+    { input: 'DRRRDD', expected: 'Radiant' },
+    { input: 'DDRRRR', expected: 'Radiant' },
+    { input: 'RRDDD', expected: 'Radiant' },
+    { input: 'RDD', expected: 'Dire' },
+    { input: 'DDRRR', expected: 'Dire' },
+    { input: 'D', expected: 'Dire' },
+  ];
+
+  testCases.forEach(({ input, expected }) => {
+    it(`should return "${expected}" for input "${input}"`, () => {
+      const result = predictPartyVictory(input);
+      expect(result).toBe(expected);
+    });
+  });
+});
---
+++ problems/649. Dota2 Senate/index.ts
@@ -0,0 +1,52 @@
+// O(n^2)
+function predictPartyVictory1(senate: string): string {
+  const senateMembers = senate.split('');
+
+  while (senateMembers.length > 1) {
+    const currentMember = senateMembers.shift() as string;
+
+    let isFinish = true;
+    for (let i = 0; i < senateMembers.length - 1; i++) {
+      if (senateMembers[i] !== currentMember && senateMembers[i] !== 'banned') {
+        senateMembers.splice(i, 1);
+        isFinish = false;
+        break;
+      }
+    }
+
+    senateMembers.push(currentMember);
+    if (isFinish) break;
+  }
+
+  return senateMembers[senateMembers.length - 1] === 'R' ? 'Radiant' : 'Dire';
+}
+
+// O(n)
+function predictPartyVictory(senate: string): string {
+  const n = senate.length;
+  const rQueue = [];
+  const dQueue = [];
+
+  for (let i = 0; i < n; i++) {
+    if (senate[i] === 'R') {
+      rQueue.push(i);
+    } else {
+      dQueue.push(i);
+    }
+  }
+
+  while (rQueue.length && dQueue.length) {
+    const rTurn = rQueue.shift() as number;
+    const dTurn = dQueue.shift() as number;
+
+    if (dTurn < rTurn) {
+      dQueue.push(dTurn + n);
+    } else {
+      rQueue.push(rTurn + n);
+    }
+  }
+
+  return rQueue.length === 0 ? 'Dire' : 'Radiant';
+}
+
+export { predictPartyVictory };
