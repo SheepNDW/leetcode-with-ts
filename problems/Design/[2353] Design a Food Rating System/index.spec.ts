63d9d89a3260fc3c52070f05f10b3e62f828762e
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-12-31 23:32:14 +0800

    chore: update 2353

---
+++ problems/Design/[2353] Design a Food Rating System/index.spec.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it } from 'vitest';
+import { FoodRatings } from '.';
+
+describe.skip('FoodRatings', () => {
+  it('should correctly initialize and retrieve highest rated food', () => {
+    const foodRatings = new FoodRatings(
+      ['kimchi', 'miso', 'sushi', 'moussaka', 'ramen', 'bulgogi'],
+      ['korean', 'japanese', 'japanese', 'greek', 'japanese', 'korean'],
+      [9, 12, 8, 15, 14, 7]
+    );
+
+    expect(foodRatings.highestRated('korean')).toBe('kimchi');
+    expect(foodRatings.highestRated('japanese')).toBe('ramen');
+  });
+
+  it('should update the rating and reflect the highest rated food correctly', () => {
+    const foodRatings = new FoodRatings(
+      ['kimchi', 'miso', 'sushi', 'moussaka', 'ramen', 'bulgogi'],
+      ['korean', 'japanese', 'japanese', 'greek', 'japanese', 'korean'],
+      [9, 12, 8, 15, 14, 7]
+    );
+
+    foodRatings.changeRating('sushi', 16);
+    expect(foodRatings.highestRated('japanese')).toBe('sushi');
+
+    foodRatings.changeRating('ramen', 16);
+    expect(foodRatings.highestRated('japanese')).toBe('ramen');
+  });
+});
