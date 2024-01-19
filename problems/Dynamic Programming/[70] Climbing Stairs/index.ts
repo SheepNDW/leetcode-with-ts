a89d1acd56c9401cddc75d75fac5d0d1fe72b2d9
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2024-01-19 08:57:59 +0800

    chore: update alternative solution

--- problems/Dynamic Programming/[70] Climbing Stairs/index.ts
+++ problems/Dynamic Programming/[70] Climbing Stairs/index.ts
@@ -1,9 +1,31 @@
-function climbStairs(n: number): number {
-  const dp = [0, 1, 2];
-  for (let i = 3; i <= n; i++) {
+function climbStairs0(n: number): number {
+  // dp[i]: ways to climb i steps
+  // dp[0] = 1, dp[1] = 1, dp[2] = 2, dp[3] = 3, dp[4] = 5
+  // dp[i] = dp[i - 1] + dp[i - 2]
+  const dp = Array.from({ length: n + 1 }, () => 0);
+  dp[0] = 1;
+  dp[1] = 1;
+
+  for (let i = 2; i <= n; i++) {
     dp[i] = dp[i - 1] + dp[i - 2];
   }
+
   return dp[n];
+}
+
+// space optimized
+function climbStairs(n: number): number {
+  if (n === 1) return 1;
+
+  let a = 1;
+  let b = 2;
+  for (let i = 3; i <= n; i++) {
+    const curr = a + b;
+    a = b;
+    b = curr;
+  }
+
+  return b;
 }
 
 export { climbStairs };
