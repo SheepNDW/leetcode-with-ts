dab2497294da27a6a8f9aaef8d76f507cebaa1e4
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2024-07-09 08:59:13 +0800

    chore: update 1823

--- problems/Queue/[1823] Find the Winner of the Circular Game/index.ts
+++ problems/Queue/[1823] Find the Winner of the Circular Game/index.ts
@@ -14,8 +14,8 @@ function findTheWinner0(n: number, k: number): number {
   return playersQueue[0];
 }
 
-// queue class
-function findTheWinner(n: number, k: number): number {
+// queue class: TC: O(n * k)
+function findTheWinner1(n: number, k: number): number {
   const playersQueue = new Queue<number>();
 
   for (let i = 1; i <= n; i++) {
@@ -30,6 +30,37 @@ function findTheWinner(n: number, k: number): number {
   }
 
   return playersQueue.front();
+}
+
+/*
+  Recursion: f(n,k) = (f(n-1,k) + k) mod n
+
+  TC: O(n)
+  SC: O(n)
+*/
+function findTheWinner2(n: number, k: number): number {
+  const helper = (n: number, k: number): number => {
+    if (n === 1) return 0; // 基底情況：只有一個人時，他的位置是 0（0-indexed）
+    return (helper(n - 1, k) + k) % n;
+  };
+
+  return helper(n, k) + 1; // 將結果轉換為 1-indexed
+}
+
+/*
+  Iterative
+
+  TC: O(n)
+  SC: O(1)
+*/
+function findTheWinner(n: number, k: number): number {
+  let winner = 0;
+
+  for (let i = 2; i <= n; i++) {
+    winner = (winner + k) % i;
+  }
+
+  return winner + 1;
 }
 
 export { findTheWinner };
