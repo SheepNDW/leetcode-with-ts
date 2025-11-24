f6a4139d2ebe053cd15edbefea1d3805615df58a
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2025-11-24 09:53:09 +0800

    chore: update
    rewrite

--- problems/String/[1930] Unique Length-3 Palindromic Subsequences/index.spec.ts
+++ problems/String/[1930] Unique Length-3 Palindromic Subsequences/index.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import { countPalindromicSubsequence } from '.';
 
-describe.skip('countPalindromicSubsequence', () => {
+describe('countPalindromicSubsequence', () => {
   it('should return 3 when given "aabca"', () => {
     const s = 'aabca';
 
--- problems/String/[1930] Unique Length-3 Palindromic Subsequences/index.ts
+++ problems/String/[1930] Unique Length-3 Palindromic Subsequences/index.ts
@@ -3,7 +3,7 @@
  * 2. 字串中最多有 26 個不重複的小寫字母，我們檢查其首次出現和最後一次出現之間的所有 unique 字母。
  *    最差情況下會對整個字串進行遍歷，時間複雜度為 O(26n)，可以簡化為 O(n)
  */
-function countPalindromicSubsequence(s: string): number {
+function countPalindromicSubsequence0(s: string): number {
   const n = s.length;
   const firstOccurrence = new Map<string, number>();
   const lastOccurrence = new Map<string, number>();
@@ -26,6 +26,34 @@ function countPalindromicSubsequence(s: string): number {
   });
 
   return count;
+}
+
+/*
+  use array to record first and last occurrence index
+*/
+function countPalindromicSubsequence(s: string): number {
+  const n = s.length;
+  const first = new Array(26).fill(-1);
+  const last = new Array(26).fill(-1);
+
+  for (let i = 0; i < n; i++) {
+    const idx = s.charCodeAt(i) - 'a'.charCodeAt(0);
+    if (first[idx] === -1) first[idx] = i;
+    last[idx] = i;
+  }
+
+  let res = 0;
+
+  for (let c = 0; c < 26; c++) {
+    if (first[c] === -1 || first[c] === last[c]) continue;
+    const set = new Set<string>();
+    for (let j = first[c] + 1; j < last[c]; j++) {
+      set.add(s[j]);
+    }
+    res += set.size;
+  }
+
+  return res;
 }
 
 export { countPalindromicSubsequence };
